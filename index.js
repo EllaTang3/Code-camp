cf13608fb2f66c16194d3d9dd5f10e6ca8db01f7
EllaTang3 <ella.tang130@gmail.com>
2025-07-03 14:01:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express'); 
+xconst express = require('express'); 
 const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname))
 
 app.get('/', (req, res)=> {
-  res.sendFile(__dirname + '/stuff.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/other', (req, res)=> {
-  res.send(__dirname + '/stuff.html');
+  res.send(__dirname + '/index.html');
 });
 
 app.get('/thing', (req, res)=> {
-  res.send(__dirname + '/stuff.html');
+  res.send(__dirname + '/index.html');
 });
 
 app.listen(port, () => {
