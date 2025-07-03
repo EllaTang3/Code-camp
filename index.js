ab4a0fb0450583272386af6ba4e1e3a0e772bd40
EllaTang3 <ella.tang130@gmail.com>
2025-07-03 14:13:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res)=> {
   res.sendFile(__dirname + '/index.html');
 });
 
+app/.get('/index', (req, res)=> {
+  res.sendFile(__dirname + 'index.html');
+});
+
 app.get('/other', (req, res)=> {
   res.send(__dirname + '/index.html');
 });
